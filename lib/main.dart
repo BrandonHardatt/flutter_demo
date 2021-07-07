d16b3fc7dac8f10ab94ef23ff39669cc0ce21a67
BrandonHardatt <72095737+BrandonHardatt@users.noreply.github.com>
2021-07-07 21:17:36 -0230

    Made a main screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,6 @@
 import 'package:flutter/material.dart';
+import 'app.dart';
 
 void main() {
-  var header = HeaderWidget();
-  header.renderHeader();
-  var body = BodyWidget();
-  body.renderBody();
-}
-
-class HeaderWidget {
-  var height = 100;
-  var title = 'Earth';
-
-  renderHeader() {
-    print("Hello from earth.");
-  }
-}
-
-class BodyWidget {
-  renderBody() {
-    print("Hello from mars.");
-  }
+  runApp(App());
 }
