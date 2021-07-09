c367b951408073e1b9ccc38378bbdf1a98e2554d
Brandon Hardatt <brandonhardatt@Brandons-MacBook-Air.local>
2021-07-09 11:08:59 -0230

    Changes to import

--- lib/app.dart
+++ lib/app.dart
@@ -1,4 +1,5 @@
-import 'package:flutter/material.dart';
+import 'package:flutter/material.dart'
+    show BuildContext, MaterialApp, StatelessWidget, Widget;
 import 'welcome.dart';
 
 class App extends StatelessWidget {
