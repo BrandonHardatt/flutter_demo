69e44679e96a7c5f873a1fd7e36c7efc2310c92f
Brandon Hardatt <brandonhardatt@Brandons-MacBook-Air.local>
2021-07-09 23:58:03 -0230

    Big changes

--- lib/app.dart
+++ lib/app.dart
@@ -3,7 +3,6 @@ import 'welcome.dart';
 
 class App extends StatelessWidget {
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(home: Welcome());
-  }
+  Widget build(BuildContext context) =>
+      MaterialApp(debugShowCheckedModeBanner: false, home: Welcome());
 }
--- lib/welcome.dart
+++ lib/welcome.dart
@@ -5,9 +5,25 @@ class Welcome extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-            title: Text('Hello, World?'),
+            title: Text('Appointment Booking', style: TextStyle(fontSize: 24)),
             centerTitle: true,
-            backgroundColor: Colors.amberAccent),
-        body: Center(child: Text("Hello Mars!")));
+            backgroundColor: Colors.orangeAccent.shade700,
+            actions: <Widget>[
+              IconButton(
+                  color: Colors.blue,
+                  onPressed: () => debugPrint("Email pressed."),
+                  icon: Icon(Icons.calendar_today)),
+            ]),
+        backgroundColor: Colors.amberAccent.shade700,
+        body: Container(
+            alignment: Alignment.center,
+            child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  InkWell(
+                      child: Text("Book a new Appointment",
+                          style: TextStyle(fontSize: 24)),
+                      onTap: () => debugPrint("New Appointment booked."))
+                ])));
   }
 }
